e58baa6d6f03b1671ae6a15d3f609b6197c5f7ce
Your Name <you@example.com>
2020-07-01 15:44:15 +0900

    로그인 페이지5

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ require('ejs')
 var apiRouter = require('./routes/Router')
 const mongoose = require('mongoose');
 
+mongoose.Promise=global.Promise
+var flash = require('express-flacsh-message')
+// var passport = require('express-session')
+var session = require('express-session')
+
 
 var pw = process.env.PASSWORD
 var url = `mongodb+srv://root:${pw}@cluster0-tecvg.mongodb.net/mydb_daejeon?retryWrites=true&w=majority`
@@ -16,14 +21,60 @@ app.set('views',path.resolve(__dirname + '/view'))
 app.set('view engine', 'ejs')
 
 var bodyParser = require('body-parser')
+const passport = require('passport')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret:"그러하다"  ,
+    resave:true,
+    saveUninitialized:true
+}))
+app.use(flash())
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser((user, done)=>{
+    done(null, user._id)
+})
+
+passport.deserializeUser((user)=>{
+    User.findById(userId, (err, user)=>{
+        done(err,user)
+    })
+})
+
 
 // app.get('/',(req, res)=>{
 //     res.render('test')
 // })  
-app.use('/',apiRouter)
+var localStrategy = require("passport-local")
+var local = new localStrategy((username, password, done)=>{
+    User.findOne({username})
+    .then(user=>{
+        if(!user ||user.validPassword(password)){
+            done(null, false, {message:"Invalid username /password"})
+        } else {
+            done(null, user)
+        }
+    })
+    .catch(e=>done(e))
+});
+
+passport.use("local",local)
+
+
+
+app.use('/',apiRouter(passport)){
+    router.use((req, res, next)=>{
+        res.locals.currentUser = req.user
+        console.log(res.locals.currentUser)
+        next()
+    })
+
+}
 
 const port = process.env.PORT
 
